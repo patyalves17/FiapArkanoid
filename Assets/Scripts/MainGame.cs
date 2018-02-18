864b069bb0e998ed6ad897bd82848bab3d9ac1b8
ABC Education <abceducation@AirAbc.local>
2018-02-18 18:05:06 -0300

    arkanoid

---
+++ Assets/Scripts/MainGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainGame : MonoBehaviour {
+
+	public static int blocks;
+	public static float score;
+	public Text txtScore;
+
+	// Use this for initialization
+	void Start () {
+		blocks = 45;
+		score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (blocks <= 0) {
+			SceneManager.LoadScene ("Menu");
+		}
+		print (score);
+		txtScore.text = score.ToString ();
+	}
+}
